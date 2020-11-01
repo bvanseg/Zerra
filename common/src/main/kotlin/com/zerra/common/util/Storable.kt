18e303c0c82e9fc575a43d0d0634fac872b70c09
bvanseg <bvanseg@gmail.com>
2020-11-01 15:10:03 -0500

    Added a Storable interface.

---
+++ common/src/main/kotlin/com/zerra/common/util/Storable.kt
@@ -0,0 +1,14 @@
+package com.zerra.common.util
+
+import com.devsmart.ubjson.UBObject
+
+/**
+ * An interface representing an object that can be written to and read from the Universal Binary JSON (UBJSON) format.
+ *
+ * @author Boston Vanseghi
+ * @since 0.0.1
+ */
+interface Storable {
+    fun read(): UBObject
+    fun write(ubj: UBObject)
+}
