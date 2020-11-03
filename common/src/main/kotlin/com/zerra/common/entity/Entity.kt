b3ff19a23f0e866f8b14d7a7a342002c8c94252e
bvanseg <bvanseg@gmail.com>
2020-11-03 14:00:58 -0500

    Added position and velocity to entities.

--- common/src/main/kotlin/com/zerra/common/entity/Entity.kt
+++ common/src/main/kotlin/com/zerra/common/entity/Entity.kt
@@ -2,6 +2,7 @@ package com.zerra.common.entity
 
 import com.zerra.common.util.storage.Storable
 import com.zerra.common.util.storage.UBJ
+import org.joml.Vector3f
 import java.util.*
 
 /**
@@ -14,6 +15,8 @@ abstract class Entity: Storable {
 
     var uuid: UUID = UUID.randomUUID()
     var ticksExisted: Long = 0
+    var position: Vector3f = Vector3f(0f, 0f, 0f)
+    var velocity: Vector3f = Vector3f(0f, 0f, 0f)
 
     fun update() {
         ticksExisted++
@@ -22,12 +25,16 @@ abstract class Entity: Storable {
     override fun read(ubj: UBJ) {
         this.uuid = ubj.readUUIDOrDefault("uuid", UUID.randomUUID())
         this.ticksExisted = ubj.readLongOrDefault("ticksExisted", 0)
+        this.position = ubj.readVector3fOrDefault("pos", Vector3f(0f, 0f, 0f))
+        this.velocity = ubj.readVector3fOrDefault("vel", Vector3f(0f, 0f, 0f))
     }
 
     override fun write(): UBJ {
         val ubj = UBJ()
         ubj.put("uuid", uuid)
         ubj.put("ticksExisted", ticksExisted)
+        ubj.put("pos", position)
+        ubj.put("vel", velocity)
 
         return ubj
     }
