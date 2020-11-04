7bc04ceabd7ebab87df4ba3e0297ce90845c6911
bvanseg <bvanseg@gmail.com>
2020-11-04 11:01:59 -0500

    Added bulk of chunk class.

--- common/src/main/kotlin/com/zerra/common/realm/chunk/Chunk.kt
+++ common/src/main/kotlin/com/zerra/common/realm/chunk/Chunk.kt
@@ -4,21 +4,100 @@ import bvanseg.kotlincommons.number.HashCodeBuilder
 import com.zerra.common.block.Block
 import com.zerra.common.block.Blocks
 import com.zerra.common.realm.Realm
+import com.zerra.common.util.storage.Storable
+import com.zerra.common.util.storage.UBJ
+import org.joml.Vector3i
+import org.joml.Vector3ic
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * @author Boston Vanseghi
  * @since 0.0.1
  */
-class Chunk(val realm: Realm, val posX: Int, val posY: Int, val posZ: Int) {
+class Chunk(val realm: Realm, val posX: Int, val posY: Int, val posZ: Int): Storable {
 
     companion object {
         const val CHUNK_SIZE = 16 * 16 * 16
     }
 
-    private val blocks = Array(CHUNK_SIZE) { Blocks.AIR }
+    private val changes by lazy { ConcurrentHashMap<Vector3ic, Block>() }
+    private val blockStates = Array(CHUNK_SIZE) { Blocks.AIR } // TODO: Store BlockState, not Block
 
-    fun getBlock(x: Int, y: Int, z: Int) = blocks[x + y * 16 + z * 256]
-    fun setBlock(block: Block, x: Int, y: Int, z: Int) = blocks[x + y * 16 + z * 256]
+    fun getBlockState(x: Int, y: Int, z: Int) = blockStates[x + y * 16 + z * 256]
+    fun setBlockState(blockState: Block, x: Int, y: Int, z: Int) {
+        val currentBlockState = getBlockState(x, y, z)
+
+        if (blockState == currentBlockState) return
+
+        changes[Vector3i(x, y, z)] = blockState
+        blockStates[x + y * 16 + z * 256]
+    }
 
     override fun hashCode(): Int = HashCodeBuilder(this::class).append(realm).append(posX).append(posY).append(posZ).hashCode()
+
+    override fun read(ubj: UBJ) = synchronized(blockStates) {
+        val posData = ubj.readByteArray("posData")
+        val blockData = ubj.readShortArray("blockData")
+        val blockStateData = ubj.readByteArray("blockStateData")
+
+        var posIndex = 0
+        var blockIndex = 0
+        var blockStateIndex = 0
+
+        blockData.forEach { _ ->
+            val x = posData[posIndex]
+            val y = posData[posIndex + 1]
+            val z = posData[posIndex + 2]
+
+            val blockId = blockData[blockIndex]
+            val blockStateId = blockStateData[blockStateIndex]
+
+            // TODO: Set block state in blocks array for chunk here.
+
+            posIndex += 3
+            blockIndex++
+            blockStateIndex++
+        }
+    }
+
+    override fun write(): UBJ {
+        val ubj = UBJ()
+
+        val posData = ByteArray(changes.size * 3)
+        val blockData = ShortArray(changes.size)
+        val blockStateData = ByteArray(changes.size)
+
+        var posIndex = 0
+        var blockIndex = 0
+        var blockStateIndex = 0
+        changes.forEach { (pos, block) ->
+            posData[posIndex] = pos.x().toByte()
+            posData[posIndex + 1] = pos.y().toByte()
+            posData[posIndex + 2] = pos.z().toByte()
+
+            // blockData[blockIndex] = block.hashCode()
+            // blockStateData[blockStateIndex] = block.hashCode()
+
+            posIndex += 3
+            blockIndex++
+            blockStateIndex++
+        }
+
+        ubj.putByteArray("posData", posData)
+        ubj.putShortArray("blockData", blockData)
+        ubj.putByteArray("blockStateData", blockStateData)
+
+        return ubj
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if(other !is Chunk) return false
+
+        if (realm != other.realm) return false
+        if (posX != other.posX) return false
+        if (posY != other.posY) return false
+        if (posZ != other.posZ) return false
+
+        return true
+    }
 }
