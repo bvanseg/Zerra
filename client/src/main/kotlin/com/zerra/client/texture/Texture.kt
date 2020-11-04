eb582ffab5c21502f08ddc515f4e1fded5315aa1
Ocelot <FinntheRaider@gmail.com>
2020-11-03 17:28:30 -0800

    Started adding texture loading

--- client/src/main/kotlin/com/zerra/client/texture/Texture.kt
+++ client/src/main/kotlin/com/zerra/client/texture/Texture.kt
@@ -2,7 +2,10 @@ package com.zerra.client.texture
 
 import com.zerra.client.util.Bindable
 import com.zerra.common.util.resource.ResourceManager
+import java.util.concurrent.CompletableFuture
 
 interface Texture : Bindable {
-    fun load(resourceManager: ResourceManager, textureManager: TextureManager)
+    fun load(resourceManager: ResourceManager, textureManager: TextureManager): CompletableFuture<Void>
+
+    fun s(): CompletableFuture<Void>
 }
--- client/src/main/kotlin/com/zerra/client/texture/TextureManager.kt
+++ client/src/main/kotlin/com/zerra/client/texture/TextureManager.kt
@@ -1,9 +1,15 @@
 package com.zerra.client.texture;
 
+import com.zerra.common.util.resource.ResourceLocation
 import com.zerra.common.util.resource.ResourceManager
+import java.util.concurrent.CompletableFuture
 
 class TextureManager(private val resourceManager: ResourceManager) {
-    fun load() {
 
+    private val textures = HashMap<ResourceLocation, Texture>()
+
+    // TODO make async
+    fun load() {
+        CompletableFuture.allOf(*textures.values.stream().map(Texture::s).toArray { size -> arrayOfNulls<CompletableFuture<Void>>(size) }).join()
     }
 }
