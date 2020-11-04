0558e76b6ed42b3dbd1eba0a6157bde03cf54d7a
bvanseg <bvanseg@gmail.com>
2020-11-04 11:32:38 -0500

    Added function to get current save folder.

--- common/src/main/kotlin/com/zerra/common/util/storage/FileManager.kt
+++ common/src/main/kotlin/com/zerra/common/util/storage/FileManager.kt
@@ -1,5 +1,8 @@
 package com.zerra.common.util.storage
 
+import com.zerra.common.Zerra
+import com.zerra.common.realm.Universe
+import com.zerra.common.state.GameState
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.stream.Stream
@@ -11,6 +14,13 @@ import java.util.stream.Stream
 internal object FileManager {
 
     val ROOT_DIR = Path.of("data")
+
+    val SAVES_DIR by lazy {
+        val path = ROOT_DIR.resolve("saves")
+        Files.createDirectories(path)
+        path
+    }
+
     val MODS_DIR: Path by lazy {
         val path = ROOT_DIR.resolve("mods")
         Files.createDirectories(path)
@@ -18,4 +28,14 @@ internal object FileManager {
     }
 
     fun getModJARs(): Stream<Path> = Files.walk(MODS_DIR).filter(Files::isRegularFile)
+
+    fun getCurrentSaveFolder(): Path {
+        val zerra = Zerra.getInstance()
+        val state = zerra.getStateManager().activeState
+
+        if (state !is GameState) throw IllegalStateException("Attempted to fetch save folder while outside of game state!")
+        val universe = state.universe
+
+        return SAVES_DIR.resolve(universe.name)
+    }
 }
